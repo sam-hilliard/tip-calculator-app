802c21892a460de139b1f8af36ffd54676776bbf
Sam Hilliard <samhilliard51@gmail.com>
2022-12-14 18:07:42 -0800

    structure for react app

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      
+
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://sam-hilliard.github.io/">Your Name Here</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
