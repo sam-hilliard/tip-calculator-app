cc01d08abd687f785488836313376c7ca5a490d9
Sam Hilliard <samhilliard51@gmail.com>
2022-12-17 18:52:59 -0800

    app container styles and global vars

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import TipCalculator from '../TipCalculator/TipCalculator';
+import logo from '../../assets/images/logo.svg';
 import Attribution from '../Attribution/Attribution';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="Splitter" />
       <TipCalculator />
       <Attribution />
     </div>
