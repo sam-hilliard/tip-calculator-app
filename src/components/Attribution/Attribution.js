386af5b55c1dff940c2e4a2da9825240d72a2f49
Sam Hilliard <samhilliard51@gmail.com>
2022-12-19 20:36:02 -0800

    styled attribution

--- src/components/Attribution/Attribution.js
+++ src/components/Attribution/Attribution.js
@@ -4,7 +4,7 @@ export default function Attribution() {
   return (
     <div class="Attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="https://sam-hilliard.github.io/">Your Name Here</a>.
+        Coded by <a href="https://sam-hilliard.github.io/">Sam Hilliard</a>.
     </div>
   )
 }
