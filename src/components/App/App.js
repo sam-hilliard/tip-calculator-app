2ee5c521f62c2279d423e59e2fc79916011be6e0
Sam Hilliard <samhilliard51@gmail.com>
2022-12-20 18:46:40 -0800

    added context provider for tip parameters

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import TipCalculator from '../TipCalculator/TipCalculator';
 import logo from '../../assets/images/logo.svg';
 import Attribution from '../Attribution/Attribution';
+import { TipParamsContextProvider } from '../../contexts/TipParamsContext';
 
 function App() {
   return (
     <div className="App">
       <div className="container--centered">
         <img src={logo} alt="Splitter" />
-        <TipCalculator />
+        <TipParamsContextProvider>
+          <TipCalculator />
+        </TipParamsContextProvider>
       </div>
       <Attribution />
     </div>
