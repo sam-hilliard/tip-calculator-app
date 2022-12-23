e1a53d20bc1202e54fed099342b4d430088726ed
Sam Hilliard <samhilliard51@gmail.com>
2022-12-22 20:24:55 -0800

    button active states

--- src/components/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Button(props) {
-  return (
-    <button className="btn" value={props.value}>{props.title}</button>
-  )
-}
--- src/components/TipSelection/TipSelection.js
+++ src/components/TipSelection/TipSelection.js
@@ -1,22 +1,61 @@
-import React from 'react'
-
-import Button from '../Button/Button';
+import React from 'react';
+import { useState } from 'react';
 
 export default function TipSelection() {
 
-    const tipVals = [5, 10, 15, 25, 50];
+    const [btns, setBtns] = useState([
+        {
+            val: 5,
+            active: false
+        },
+        {
+            val: 10,
+            active: false
+        },
+        {
+            val: 15,
+            active: false
+        },
+        {
+            val: 25,
+            active: false
+        },
+        {
+            val: 50,
+            active: false
+        }
+    ]);
+    
+
+    function handleClick(e) {
+        const clickedVal = Number(e.target.value);
+        let newBtns = btns.map(btn => {
+            return (
+                {
+                    ...btn,
+                    active: btn.val === clickedVal
+                }
+            );
+        });
+
+        setBtns(newBtns);
+    }
 
     return (
         <div className="tip-selection-container">
             <label className="tip-selection__lbl lbl">Select Tip %</label>
 
             <div className="tip-selection__btn-container">
-                {tipVals.map(val => {
+                {btns.map((btn, index) => {
                     return (
-                        <Button 
-                            val={val / 100} 
-                            title={`${val}%`} 
-                        />
+                        <button
+                            key={index}
+                            className={`btn ${btn.active ? 'btn--light' : ''}`} 
+                            value={btn.val}
+                            onClick={handleClick}
+                        >
+                            {`${btn.val}%`}
+                        </button>
                     );
                 })}
             </div>
