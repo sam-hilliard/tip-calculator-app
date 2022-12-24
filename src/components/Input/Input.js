3bc20e3726e6ad4d595793389c0ed25e3ee3cbe9
Sam Hilliard <samhilliard51@gmail.com>
2022-12-23 20:00:09 -0800

    refactored inputs to numerical inputs

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
 import { useTipParamsContext } from '../../contexts/TipParamsContext';
 
 export default function Input(props) {
 
     const [tipParams, setTipParams] = useTipParamsContext();
+    const [errorMsg, setErrorMsg] = useState('');
 
-    const handleChange = (e) => {      
+    const handleChange = (e) => { 
+        const name = e.target.name;
+        const value = Number(e.target.value);
+
         setTipParams(prevVal => {
             return {
                 ...prevVal,
-                [e.target.name]: Number(e.target.value)
+                [name]: value
             };
         });
 
@@ -17,9 +22,13 @@ export default function Input(props) {
 
     return (
         <div className="input-container">
-            <label className="lbl input__lbl">{props.label}</label>
+            <div className="input__lbl-container">
+                <label className="lbl input__lbl">{props.label}</label>
+                <label className="lbl lbl--error input__lbl">{errorMsg}</label>
+            </div>
             <input 
                 className="input"
+                type="number"
                 name={props.name}
                 onChange={handleChange} 
                 value={tipParams[props.name]}
